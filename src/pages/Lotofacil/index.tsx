3aa55419633dcd168a64b0d93e17fc11e959cf97
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-01 19:45:25 -0300

    feat: new page Lotofacil

---
+++ src/pages/Lotofacil/index.tsx
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+import TrevoLotoFacil from '../../assets/trevo-lotofacil.png';
+import { Titulo } from "../../components/Titulo";
+import numeral from "numeral";
+import { Descricao } from "../../components/Descricao";
+import { Divisoria } from "../../components/Divisoria";
+import { useContexto } from "../../hooks";
+import { LoteriaProps } from "../../types";
+import { ListaNumerosLotoFacil } from "../../components/ListaNumerosLotoFacil";
+
+export default function Lotofacil () {
+    const {lotofacil} = useContexto();
+
+    const acumulou = (sena: LoteriaProps) => {
+        if (sena.acumulado) {
+            return 'ACUMULOU!';
+        }
+        return `${sena.quantidadeGanhadores} GANHADORES`
+    }
+    
+    return(
+        <>
+            <ResultadoStl>
+                {/* lotofacil */}
+                <LeftStl>
+                    <TituloStl>
+                        <LogoStl src={TrevoLotoFacil} alt='Mega-sena' />
+                        <Titulo cor="roxo" tamanho="grande">LOTOFÁCIL</Titulo>
+                    </TituloStl>
+                    <LeftContentStl>
+                        <Descricao>
+                            {`Estimativa de prêmio do próximo concurso. Sorteio em: ${lotofacil.dataProximoConcurso}`}
+                        </Descricao>
+                        <ValorStl>
+                            <Titulo cor="roxo" tamanho="medio">
+                                {`R$${numeral(lotofacil.valorEstimadoProximoConcurso).format('0,0.00')}`}
+                            </Titulo>
+                        </ValorStl>
+                    </LeftContentStl>
+                </LeftStl>
+                <RightStl>
+                    <ListaNumerosLotoFacil lista={lotofacil.dezenas} />
+                    <GanhadoresStl>
+                        <Titulo cor="azul" tamanho="grande">
+                            {acumulou(lotofacil)}
+                        </Titulo>
+                    </GanhadoresStl>
+                    <GanhadoresStl>
+                        <Descricao>{`Concurso ${lotofacil.numeroDoConcurso} - ${lotofacil.dataPorExtenso}`}</Descricao>
+                    </GanhadoresStl>
+                </RightStl>
+            </ResultadoStl>
+            <DivisoriaStl><Divisoria /></DivisoriaStl>
+        </>
+    )
+}
+
+const ResultadoStl = styled.div`
+    display: flex;
+    padding: 24px;
+    margin: 24px;
+`;
+
+const DivisoriaStl = styled.div`
+    margin: 24px;
+`;
+
+const LeftStl = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 365px;
+    margin-right: 64px;
+`;
+
+const TituloStl = styled.span`
+    display: flex;
+    align-items: center;
+    padding: 0;
+    height: 48px;
+    margin-bottom: 20px;
+`;
+
+const LeftContentStl = styled.div `
+    margin-left: 48px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const LogoStl = styled.img`
+    margin-right: 12px
+`;
+
+const ValorStl = styled.span`
+    margin-top: 12px;
+`;
+
+const RightStl = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 36%
+`;
+
+const GanhadoresStl = styled.span`
+    margin-top: 16px;
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Megasena from "./pages/Megasena";
+import Lotofacil from "./pages/Lotofacil";
 
 export default function AppRouter () {
     
@@ -7,7 +8,7 @@ export default function AppRouter () {
         <BrowserRouter>
             <Routes>
                     <Route path="/" index element={<Navigate to='/lotofacil' replace />} />
-                    <Route path='/lotofacil' index element={<span>lotofacil</span>} />
+                    <Route path='/lotofacil' index element={<Lotofacil />} />
                     <Route path='/megasena' element={<Megasena />} />
                     <Route path='/quina' element={<span>quina</span>} />tg
             </Routes>
