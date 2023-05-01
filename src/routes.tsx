a2f109298637e72a2ad951431201d510896f038f
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-01 20:55:43 -0300

    feat: created menu

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Megasena from "./pages/Megasena";
 import Lotofacil from "./pages/Lotofacil";
 import Quina from "./pages/Quina";
+import Menu from "./components/Menu";
 
 export default function AppRouter () {
     
     return(
         <BrowserRouter>
+            <Menu />
             <Routes>
                     <Route path="/" index element={<Navigate to='/lotofacil' replace />} />
                     <Route path='/lotofacil' index element={<Lotofacil />} />
